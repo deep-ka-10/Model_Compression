142672b4eb2bc3124462f1441a276acfcb1ebbd5
DEEPIKA <92143289+deep-ka-10@users.noreply.github.com>
2023-05-10 22:38:36 +0530

    Delete PruneAndQuantise directory

--- PruneAndQuantise/check_package.py
+++
@@ -1,7 +0,0 @@
-import modelcompression
-from keras.models import load_model
-
-model = load_model("C:\\Users\\HP\\Desktop\\Model Training\\ReluTrained_ReluQuantized\\model_relu.h5")
-
-pruned_model = modelcompression.prune_model(model, 0.8)
-
